969b974c252cf42d52be895df77cc1d8e81c173c
HariKarthyk <harikarthyk1407@gmail.com>
2023-12-29 08:11:12 +0530

    Add Alert functionality and modify header icons

--- FormfacadeWebview.tsx
+++ FormfacadeWebview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import WebView from 'react-native-webview';
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 
 interface FormfacadeWebviewProps {
     prefillFormFn: () => any;
@@ -62,19 +62,20 @@ const CART_HTML = `
 
 const getHeaderIcon = (headerIconColor?: string) => {
     return `
-        <div 
+        <button 
             id="cart-menu"
-            class="ml-4 inline-flex md:hidden md:ml-8 items-center justify-center whitespace-nowrap rounded-md border border-transparent r md:px-4 py-2 md:text-base font-medium text-white px-3 text-xs cursor-pointer relative
+            class="ml-4 inline-flex md:hidden md:ml-8 items-center justify-center whitespace-nowrap rounded-md border border-transparent r md:px-4 py-2 md:text-base font-medium text-white px-3 text-xs cursor-pointer relative"
+            type="button"
             onclick="showCart()"
         >
             <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25">
-                <path fill="#fff" d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM205-801h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Z"/>
+                <path fill="${headerIconColor}" d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM205-801h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Z"/>
             </svg>
             <span 
                 class="hidden bg-red-700 text-white font-extrabold text-center rounded-full h-4 w-4 items-center justify-center absolute top-1 right-1 ff-cart-count m-auto ff-cart-icon"
             >
             </span>
-        </div>
+        </button>
 
     `;
 }
@@ -88,11 +89,15 @@ const getHeaderHTML = (includeCart: boolean, headerBackgroundColor?: string, hea
     }
     return `
         <header class="ff-mobile-header">
-            <div onclick="goBackHandler()" class="ff-mobile-header-inner-container">
+            <button 
+                onclick="goBackHandler()" 
+                type="button"
+                class="ff-mobile-header-inner-container"
+            >
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" fill="${headerIconColor}" viewBox="0 -960 960 960" width="24">
                     <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/>
                 </svg>
-            </div>
+            </button>
             ${includeCart ? getHeaderIcon(headerIconColor) : ''}
         </header>
     `;
@@ -101,19 +106,41 @@ const getHeaderHTML = (includeCart: boolean, headerBackgroundColor?: string, hea
 const baseUrl = "https://neartail.com";
 
 const onSubmitDefaultHandler = () => {
-    console.log('onSubmitDefaultHandler');
+    Alert.alert(
+        'Form Submitted',
+        'Your form has been submitted successfully.',
+        [
+            {
+                text: 'Ok',
+                onPress: () => {},
+                style: 'cancel',
+            },
+        ],
+        { cancelable: false },
+    );
 };
 
 const onBackButtonDefaultHandler = () => {
-    console.log('onBackButtonDefaultHandler');
+    Alert.alert(
+        'Triggered Back Button',
+        'You have pressed back button.',
+        [
+            {
+                text: 'Ok',
+                onPress: () => {},
+                style: 'cancel',
+            },
+        ],
+        { cancelable: false },
+    );
 }
 
 const FormfacadeWebview = ({
     prefillFormFn,
     customCSS = '',
     formFacadeEmbedURL,
-    onSubmitFormHandler,
-    onGoBackHandler,
+    onSubmitFormHandler = onSubmitDefaultHandler,
+    onGoBackHandler = onBackButtonDefaultHandler,
     isFormFullScreen = true,
     includeCart = false,
     headerBackgroundColor,
@@ -121,6 +148,44 @@ const FormfacadeWebview = ({
 }: FormfacadeWebviewProps) => {
     const formFacadeWebviewRef = React.useRef(null);
 
+    if (formFacadeEmbedURL?.includes('/public/')) {
+        formFacadeEmbedURL = formFacadeEmbedURL.replace('/public/', '/include/');
+    
+        if (formFacadeEmbedURL?.includes('/home/form/')) {
+            formFacadeEmbedURL = formFacadeEmbedURL.replace('/home/form/', '/form/');
+        }
+    
+        // Save existing query parameters
+        const queryParamsStartIndex = formFacadeEmbedURL.indexOf('?');
+
+        let queryParams = '';
+    
+        if (queryParamsStartIndex !== -1) {
+            queryParams = formFacadeEmbedURL.slice(queryParamsStartIndex);
+            formFacadeEmbedURL = formFacadeEmbedURL.slice(0, queryParamsStartIndex);
+        }
+
+
+        const endsWithSlash = formFacadeEmbedURL.endsWith('/');
+    
+        if (!endsWithSlash) {
+            formFacadeEmbedURL += '/';
+        }
+    
+    
+        formFacadeEmbedURL += 'tailwind.js';
+
+        if(!queryParamsStartIndex) {
+            formFacadeEmbedURL += '?';
+        }
+    
+        // Append the original query parameters, if any
+        if (queryParams.length > 0) {
+            formFacadeEmbedURL += queryParams;
+        }
+
+        formFacadeEmbedURL += '&div=ff-compose';
+    }
 
     const handleGoBack = () => {
         if (onGoBackHandler) {
@@ -178,6 +243,7 @@ const FormfacadeWebview = ({
                 .ff-compose-parent {
                     padding: 14px;
                     padding-top: 60px;
+                    background-color: var(--ff-bgcolor);
                 }
                 .ff-mobile-header-inner-container {
                     font-weight: 600;
